3433eb56132e8c7a95c4fc64e7a9fa8b6dc7c491
kalpana-1999 <kalpanakumarithakur@gmail.com>
2026-06-15 14:42:56 +0530

    new line added in odd_even_one line file

--- odd_even_one_line.py
+++ odd_even_one_line.py
@@ -4,3 +4,7 @@ print("Even" if n%2==0 else "Odd")
 print("greater than 0" if n>0 else "less than 0")
 
 print("-----------------------")
+print("                     ")
+x,y,z= 30,90,10
+print(f"The greatest number is :{max(x,y,z)}")
+print("                         ")
