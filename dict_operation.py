1a9d40fbf0bf084d4ce1015eae7f222bb0151f78
kalpana-1999 <kalpanakumarithakur@gmail.com>
2026-06-16 14:19:49 +0530

    Dict_operation file added

---
+++ dict_operation.py
@@ -0,0 +1,11 @@
+the_dict={
+    "name":"Rahul",
+    "age": 28,
+    "City":"New Delhi"
+}
+
+print(the_dict)
+print(the_dict.keys())
+print(the_dict.values())
+the_dict["address"]="India"
+print(the_dict)
